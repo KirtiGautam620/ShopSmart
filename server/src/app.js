d41b0ae7cdfddf85f4676da2ae73d0fca88bf3e6
KIRTI G <kirtig@kirtis-MacBook-Pro.local>
2026-03-15 23:44:27 +0530

    added import  authroute

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import productRoutes from "./routes/product.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes)
 app.get('/', (req, res) => {
   res.send('ShopSmart Backend');
